fec1600d4ed87d9fc388edd405643bb53e841c12
Antonio Eduardo <antonio@emiolo.com>
2021-01-02 02:07:32 -0300

    🎉 First commit

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+v-app
+  v-app-bar(app color="primary")
+    v-toolbar-title Pizza Price Calculator
+  v-main
+    nuxt
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,88 @@
+<template lang="pug">
+v-container
+  v-form.my-3
+    v-text-field(label="Pizza Name" outlined v-model="name")
+    v-text-field(
+      label="Size"
+      outlined
+      type="number"
+      :value="size"
+      @input="size = parseFloat($event)"
+    )
+    v-text-field(
+      label="Price"
+      outlined
+      type="number"
+      :value="price"
+      @input="price = parseFloat($event)"
+    )
+    v-text-field(label="Price per cm²" readonly outlined :value="calculate(price, size)")
+    v-row.mx-0
+      v-btn(text @click="clearHistory" color="red") Clear history
+      v-spacer
+      v-btn(text @click="clear") Clear
+      v-btn(@click="save") Save
+  v-data-table.mt-8(
+    disable-pagination
+    hide-default-footer
+    :headers="headers"
+    :items="calculatedHistory"
+  )
+</template>
+
+<script>
+export default {
+  data: () => ({
+    token: 0,
+    name: '',
+    price: 0,
+    size: 0,
+    headers: [
+      { text: 'Name', value: 'name' },
+      { text: 'Price', value: 'price' },
+      { text: 'Size', value: 'size' },
+      { text: 'Price per cm²', value: 'calculated' },
+    ],
+  }),
+  computed: {
+    calculatedHistory() {
+      return this.history.map((item) => ({
+        ...item,
+        calculated: this.calculate(item.price, item.size),
+      }))
+    },
+    history: {
+      get() {
+        // eslint-disable-next-line no-unused-vars
+        const { token } = this // this line wathces for changes in the localStorage
+        return JSON.parse(localStorage.getItem('history') || '[]')
+      },
+      set(value) {
+        this.token++
+        localStorage.setItem('history', JSON.stringify(value))
+        return value
+      },
+    },
+  },
+  methods: {
+    calculate(price, size) {
+      return !size ? 0 : price / size
+    },
+    clear() {
+      this.price = 0
+      this.size = 0
+      this.name = ''
+    },
+    clearHistory() {
+      this.history = []
+    },
+    save() {
+      const { price, size, name } = this
+
+      this.clear()
+
+      this.history = [...this.history, { price, size, name }]
+    },
+  },
+}
+</script>
